ef7a57068977bfb1128cc69a201e1e0368cd7d30
Patrick Pluequett <ppluequett@gmail.com>
2023-12-06 23:40:59 +0100

    add hof file

---
+++ functional/hof.js
@@ -0,0 +1,27 @@
+/*
+sources & inspiration
+- will sentance - hard part of functional javascript - 
+*/
+
+// passing in a function as argument
+function copyArrayAndManipulate(array, fn) {
+    const output = [];
+    for (let i = 0; i < array.length; i++) {
+        output.push(fn(array[i]));
+    }
+    return output;
+}
+
+function mulitplyByTwo(x) {
+    return x * 2;
+}
+
+function generateUserName(x) {
+    return `User${x}`;
+}
+
+const result = copyArrayAndManipulate([1, 2, 3], mulitplyByTwo);
+const result2 = copyArrayAndManipulate([1, 2, 3], generateUserName);
+
+console.log('result', result);
+console.log('result2', result2);
